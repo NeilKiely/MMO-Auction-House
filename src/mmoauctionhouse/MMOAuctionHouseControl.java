3a7a8bf7a0cf8a10f79795e7216ccdc4383641f2
NeilKiely <neil-kiely@hotmail.com>
2017-11-01 16:35:44 +0000

    Added more CreditCard functionality and buy coins
    Changes:
    
    *CreditCard class
       >added getLastFourDigits()
       >Fixed Card Validation
    
    *MMOAuctionHouse Control & MenuControl
      >Added appropriate menu functions
    
    *Player class
      >added variable "private CreditCard primaryCard
      >made changes to addCreditCard() and removeCreditCard()
      >added ,ethod to check if Player has card

--- src/mmoauctionhouse/MMOAuctionHouseControl.java
+++ src/mmoauctionhouse/MMOAuctionHouseControl.java
@@ -29,9 +29,12 @@ public class MMOAuctionHouseControl {
                 case "Sell":
                     selectSell(currentPlayer);
                     break;
+                case "Payment Details":
+                    paymentDetails(currentPlayer);
+                    break;    
                 case "Buy Coins":
                     buyCoins(currentPlayer);
-                    break;    
+                    break;
                 case "Quit":
                     System.out.println("Bye bye!");
                     keepLooping = false;
@@ -121,7 +124,9 @@ public class MMOAuctionHouseControl {
         
     }
     
-    private void buyCoins(Player currentPlayer){
+    /* @author Chris & Neil
+    */
+    private void paymentDetails(Player currentPlayer){
         //System.out.println("Buying Coins");
         System.out.println("Please select or add a new Credit Card");
 		boolean keepLooping = true;
@@ -138,6 +143,9 @@ public class MMOAuctionHouseControl {
                  case "Remove Credit Card":
                      RemoveCreditCard(currentPlayer);
                     break;
+                 case "Change Primary Card":
+                     changePrimaryPaymentCard(currentPlayer);
+                    break;
                  case "Exit":
                      keepLooping = false;
                  break;
@@ -149,6 +157,8 @@ public class MMOAuctionHouseControl {
 	
     }
     
+    /* @author Chris & Neil
+    */
     private void AddCreditCard(Player currentPlayer)
     {
         Scanner in = new Scanner(System.in);
@@ -156,28 +166,181 @@ public class MMOAuctionHouseControl {
         String firstName = in.nextLine();
         System.out.println("Please enter your last name: ");
         String lastName = in.nextLine();
-        System.out.println("Please enter your card number: ");
-        String cardNo = in.nextLine();
-        System.out.println("Please enter your cards date (FORMAT MM/YY) ");
-        String expDate = in.nextLine();
-        System.out.println("Please enter your csv number: ");
-        String csvNo = in.nextLine();
-        boolean makeCard = CreditCard.validateCreditCardInformation(cardNo, csvNo, expDate);
-        if (makeCard)
+        
+        boolean cardCheck = false;
+        String cardNo = null;
+        boolean csvCheck = false;
+        String expDate = null;
+        boolean dateCheck = false;
+        String csvNo = null;
+        
+        while(!cardCheck)
         {
-            CreditCard card = new CreditCard(firstName, lastName, cardNo, expDate, csvNo);
-            currentPlayer.addCreditCard(card);
+            System.out.println("Please enter your card number: ");
+            cardNo = in.nextLine();
+            cardCheck = CreditCard.validateCreditCardInformation(cardNo);
         }
-        else
-            System.out.println("Incorrect details");
+        while(!dateCheck)
+        {
+            System.out.println("Please enter your cards date (FORMAT MM/YY) ");
+            expDate = in.nextLine();
+            dateCheck = CreditCard.vaildateDate(expDate);
+        }
+        while(!csvCheck)
+        {
+            System.out.println("Please enter your csv number: ");
+            csvNo = in.nextLine();
+            csvCheck = CreditCard.validateCsvNo(csvNo);
+        }
+        CreditCard card = new CreditCard(firstName, lastName, cardNo, expDate, csvNo);
+        currentPlayer.addCreditCard(card);
     }
     
+    /* @author Chris & Neil
+    */
     private void RemoveCreditCard(Player currentPlayer)
     {
        Scanner in = new Scanner(System.in);
        System.out.println("Please enter the number of the card you want to remove: ");
        String cardNo = in.nextLine();
        currentPlayer.removeCreditCard(cardNo);
+    }
+    
+    /* @author Chris & Neil
+    */
+    private void buyCoins(Player currentPlayer)
+    {
+        boolean keepLooping = true;
+        while (keepLooping) {
+            String selectedMenuOption = MenuControl.selectCoinOptions();
+
+            switch (selectedMenuOption) {
+                case "Buy Bronze Coins":
+                    buyBronzeCoins(currentPlayer);
+                    break;
+                 case "Buy Silver Coins":
+                    buySilverCoins(currentPlayer);
+                    break;
+                case "Buy Gold Coins":
+                    buyGoldCoins(currentPlayer);
+                    break;
+                case "Exit":
+                    keepLooping = false;
+                    break;
+                default:
+                    System.out.println("Uh oh! Something went wrong");
+                    break;
+            }
+        }
+    }
+    
+    /* @author Chris & Neil
+    */
+    private void buyBronzeCoins(Player currentPlayer)
+    {
+        boolean keepLooping = true;
+        while (keepLooping) {
+            String selectedMenuOption = MenuControl.buyBronzeCoins();
+
+            switch (selectedMenuOption) {
+                case "10 Bronze Coins (99c)":
+                    currentPlayer.getWallet().increaseAmount(10);
+                    break;
+                 case "25 Bronze Coins (\u20ac1.98)":
+                    currentPlayer.getWallet().increaseAmount(25);
+                   // System.out.println(currentPlayer.getWallet().getBronzeCoins());
+                    break;
+                case "50 Bronze Coins (\u20ac2.97)":
+                    currentPlayer.getWallet().increaseAmount(50);
+                    break;
+                case "Exit":
+                    keepLooping = false;
+                    break;
+                default:
+                    System.out.println("Uh oh! Something went wrong");
+                    break;
+            }
+        }
+    }
+    
+    /* @author Chris & Neil
+    */
+    private void buySilverCoins(Player currentPlayer)
+    {
+        boolean keepLooping = true;
+        while (keepLooping) {
+            String selectedMenuOption = MenuControl.buySilverCoins();
+
+            switch (selectedMenuOption) {
+                case "10 Silver Coins (\u20ac8.99)":
+                   currentPlayer.getWallet().increaseAmount(0, 10);
+                    break;
+                case "25 Silver Coins (\u20ac10.99)":
+                   currentPlayer.getWallet().increaseAmount(0, 25);
+                    break;
+                case "50 Silver Coins (\u20ac12.99)":
+                   currentPlayer.getWallet().increaseAmount(0, 50);
+                    break;
+                case "Exit":
+                    keepLooping = false;
+                    break;
+                default:
+                    System.out.println("Uh oh! Something went wrong");
+                    break;
+            }
+        }
+    }
+    
+    /* @author Chris & Neil
+    */
+    private void buyGoldCoins(Player currentPlayer)
+    {
+        boolean keepLooping = true;
+        while (keepLooping) {
+            String selectedMenuOption = MenuControl.buyGoldCoins();
+
+            switch (selectedMenuOption) {
+                case "10 Gold Coins (\u20ac15.99)":
+                   currentPlayer.getWallet().increaseAmount(0,0,10);
+                    break;
+                 case "25 Gold Coins(\u20ac18.99)":
+                   currentPlayer.getWallet().increaseAmount(0,0,25);
+                    break;
+                case "50 Gold Coins(\u20ac20.99)":
+                   currentPlayer.getWallet().increaseAmount(0,0,50);
+                    break;
+                case "Exit":
+                    keepLooping = false;
+                    break;
+                default:
+                    System.out.println("Uh oh! Something went wrong");
+                    break;
+            }
+        }
+    }
+    
+    private void changePrimaryPaymentCard(Player currentPlayer)
+    {
+        boolean keepLooping = false;
+        if (currentPlayer.checkHasCard()) keepLooping = true;
+        if (keepLooping == false)
+            System.out.println("No cards found!");
+        while (keepLooping) {
+            String selectedMenuOption = MenuControl.selectPrimaryCreditCard(currentPlayer.getCreditCardsStringArray());
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (currentPlayer.getCreditCard()[i] != null)
+                {
+                    if (selectedMenuOption.equals(currentPlayer.getCreditCard()[i].getCardNo().substring(currentPlayer.getCreditCard()[i].getCardNo().length() - 4)))
+                    {
+                        currentPlayer.setPrimaryCard(currentPlayer.getCreditCard()[i]);
+                        keepLooping = false;
+                        System.out.print("Primary card set!");
+                    }
+                }
+            }
+        }
     }
 }
  
